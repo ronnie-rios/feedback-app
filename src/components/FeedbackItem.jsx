d7306eaa931ce0dc0f4900b211438cd95949264a
Ronnie Rios <ronniesmail14@gmail.com>
2022-04-15 10:36:05 -0500

    styling and card

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import Card from './shared/Card';
 function FeedbackItem ({ item }) {
   //passing item from the list component, it's already destructured
   //setting it to item.rating and .text, thus having it display
     return (
-    <div className='card'>
+    <Card reverse={true}>
         <div className='num-display'>{item.rating}</div>
         <div className='text-display'>{item.text}</div>
-     </div>
+     </Card>
   )
 }
 
