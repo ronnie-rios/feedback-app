aa129f3c27178c0e351980524d7b790d4e74c407
Ronnie Rios <ronniesmail14@gmail.com>
2022-04-19 08:12:04 -0500

    fetch function to get data from db.json and display on component

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -1,32 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'item from context 1',
-            rating: 9,
-        },
-        {
-            id: 2,
-            text: 'item from context 2',
-            rating: 8,
-        },
-        {
-            id: 3,
-            text: 'item from context 3',
-            rating: 7,
-        },
-    ])
+    const [isloading, setLoading] = useState(true)
+    const [feedback, setFeedback] = useState([ ])
     //edit state
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    useEffect(() => {
+        //run on page load fetch from api
+        fetchFeedback()
+        //blank arr = runs 1 
+    }, [])
+
+    //fetching feedback from db.json
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:4000/feedback?_sort=id&_order=desc")
+
+        const data = await response.json();
+        setFeedback(data)
+        setLoading(false)
+
+    }
     //delete feedback filter by id and remove from arr using filter
     const deleteFeedback = (id) => {
     if (window.confirm('are you sure you want to delete')) {
@@ -59,12 +59,13 @@ export const FeedbackProvider = ({ children }) => {
     return <FeedbackContext.Provider
         value={{
           feedback,
+          feedbackEdit,
+          isloading,
+          //abv is state, below is functions
           deleteFeedback,
           addFeedback,
           editFeedback,
           updateFeedback,
-          //feedbackedit is state line above is the func
-          feedbackEdit
         }}>
         { children }
         </FeedbackContext.Provider>
